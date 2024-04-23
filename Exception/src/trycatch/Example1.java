b23fcecc39e13f6e63554e286c15eb94b0aa41ff
ahsbth <asraj48342@gmail.com>
2024-04-23 14:31:36 +0530

    intial commit

--- Exception/src/trycatch/Example1.java
+++ Exception/src/trycatch/Example1.java
@@ -1,14 +1,25 @@
 package trycatch;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Example1 {
     public static void main(String args[]){
-        int a=Integer.parseInt(args[0]);
-        int b=Integer.parseInt(args[1]);
+    	Scanner sc=new Scanner(System.in);
+    	
+    	
+    	
+        
         try{
+        	System.out.println("Enter First No:");
+        	int a=sc.nextInt();
+        	System.out.println("Enter Second No:");
+        	int b=sc.nextInt();
             int r=a/b;
             System.out.println("Result:"+r);
         }
-        catch(NumberFormatException e){
+        catch(InputMismatchException e) {
+        	System.out.println("Please Enter an Integer Value");
+        }
+        catch(ArithmeticException e){
             System.out.println(e.getMessage());
         }
     }
